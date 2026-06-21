ea50e2358fd2ddf78bba75a7f9d7601fb5e08657
Vijaya <vijaya2798@gmail.com>
2026-06-21 17:29:41 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+	res.write("On the way to be a full stack engineer!");
+	res.end();
+
+}).listen(3000);
+
+console.log('server started!');
